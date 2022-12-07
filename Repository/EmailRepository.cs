3f3238836aa771640b2a3afe392a55820fe495db
Konserwant <kudkacper@gmail.com>
2022-12-07 20:39:51 +0100

    missing files

---
+++ Repository/EmailRepository.cs
@@ -0,0 +1,30 @@
+﻿using CarsRent.Entities;
+using System.Net;
+using System.Net.Mail;
+
+namespace CarsRent.Repository
+{
+    public class EmailRepository : IEmailRepository
+    {
+        public void SendEmailReservation(string recipient, string subject, Reservation reservationInfo)
+        {
+            var smtpClient = new SmtpClient
+            {
+                Credentials = new NetworkCredential("kkacper909@gmail.com", "fovpjyiadybsvxmi"),
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress("kkacper909@gmail.com"),
+                Subject = subject,
+                Body = $"Testowy email"
+            };
+
+            mailMessage.To.Add(recipient);
+            smtpClient.Send(mailMessage);
+        }
+    }
+}
---
+++ Repository/IEmailRepository.cs
@@ -0,0 +1,9 @@
+﻿using CarsRent.Entities;
+
+namespace CarsRent.Repository
+{
+    public interface IEmailRepository
+    {
+        void SendEmailReservation(string recipient, string subject, Reservation reservationInfo);
+    }
+}
